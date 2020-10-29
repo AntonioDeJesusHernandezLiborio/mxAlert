fe21cf2760418d8c3c0a87eebf5ca39e00b0905d
Antonio de Jesus Hernandez Liborio <47048762+AntonioDeJesusHernandezLiborio@users.noreply.github.com>
2020-10-29 01:44:03 -0600

    QUITANDO PAGINAS Y ELEMENTOS DE LA PLATILLA
    agregando sidebar y header unicos en un solo HTML

---
+++ MxAlertFrontEnd/assets/js/app.js
@@ -0,0 +1,7 @@
+function cargarSectionPages(){
+    $("#header").load("./header.html");
+    $("#sidebar").load("./sidebar.html");
+    $("#footer").load("./footer.html");
+}
+
+cargarSectionPages();
